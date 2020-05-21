2229c51ea23de22507a93c5711781cc2dc2e689d
kudoichika <kudoichika19@gmail.com>
2020-05-21 14:46:43 -0400

    Dark Mode looks cooler

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "trackerwidget.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    TrackerWidget w;
+    w.show();
+    return a.exec();
+}
